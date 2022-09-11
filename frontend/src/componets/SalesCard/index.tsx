bbe5806edd6217d8fdb0e25bff02e066a739b7bd
Eliton lima <elitonlima2@hotmail.com>
2022-09-11 14:05:26 -0300

    Sale listing

--- frontend/src/componets/SalesCard/index.tsx
+++ frontend/src/componets/SalesCard/index.tsx
@@ -2,29 +2,33 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 import NotificationButton from '../NotificationButton';
 import './styles.css'
 
 function SalesCard() {
-//Macete para criar uma data de X dias atrás:
-const min = new Date(new Date().setDate(new Date().getDate() - 365)); 
-const max = new Date();
+    //Macete para criar uma data de X dias atrás:
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const max = new Date();
 
-// responsavel para alterar a data
-//minDate, setMinDate é um dato composto , setMinDate é a function que muda o dado.
-// é uma declaração do estado dentro do componente Reat.
-// useState(new Date()); começando com a data atual
-const [minDate, setMinDate] = useState(min);
-const [maxDate, setMaxDate] = useState(max);
+    // responsavel para alterar a data
+    //minDate, setMinDate é um dato composto , setMinDate é a function que muda o dado.
+    // é uma declaração do estado dentro do componente Reat.
+    // useState(new Date()); começando com a data atual
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
 
-useEffect(() => { 
-    axios.get("http://localhost:8080/sales")
-        .then((response) => { 
-            console.log(response.data);
-        })
-},[]  ) 
+    const [sales, setSales] = useState<Sale[]>([]);
 
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales`)
+            .then((response) => {
+                setSales(response.data.content); // atualizar o useState com o valor que retornout da API.
+            })
+    }, [])
 
+    // .map() e para percorrer a lista e faz uma operação com cada element da lista.
     return (
         <>
             <div className="dsmeta-card">
@@ -63,45 +67,26 @@ useEffect(() => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">08/07/2022</td>
-                                <td>Anakin</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
+
+                            {sales.map(sale => {
+                                return (
+                                    <tr key={sale.id}>
+                                        <td className="show992">{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>{sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <div className="dsmeta-red-btn-container">
+                                                <NotificationButton />
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+
+                            })}
+
                         </tbody>
 
                     </table>
