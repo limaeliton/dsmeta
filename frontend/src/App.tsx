739e10bc655877a84b1b99ea2d13b4a57667a8e0
Eliton lima <elitonlima2@hotmail.com>
2022-09-11 15:23:10 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./componets/Header";
 import SalesCard from "./componets/SalesCard";
 
+// ToastContainer é um componente do do react-toastify.
 function App() {
   return (
     <>
+    <ToastContainer/>
       <Header />
       <main>
         <section id="sales">
