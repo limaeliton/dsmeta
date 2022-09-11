85cec7ec4b1c153d929c02f2f62f0b7137bb34af
Eliton lima <elitonlima2@hotmail.com>
2022-09-11 15:09:59 -0300

    Send notification

--- frontend/src/componets/NotificationButton/index.tsx
+++ frontend/src/componets/NotificationButton/index.tsx
@@ -1,9 +1,25 @@
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
+import { BASE_URL } from '../../utils/request';
 import './styles.css';
 
-function NotificationButton() {
+// é um tipo.
+type Props = { 
+  saleId : number;
+}
+// axios biblioteca para fazer requisição.
+function handleClick(id : number) {
+  axios(`${BASE_URL}/sales/${id}/notification`)
+    .then(response => {
+      console.log("Success");
+    })
+}
+
+// {saleId} é o ID da venda,  Props é o tipo.
+// Props são promessas.
+function NotificationButton({saleId} : Props) {
     return (
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
         <img src={icon}alt="Notificar" />
       </div>
     )
